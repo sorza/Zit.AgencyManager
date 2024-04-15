390e9fa847fb1e47824481e90b9675fe610bf2c5
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2024-04-15 00:00:24 -0300

    Padronização de Layout de Formulários
    Separação de contextos com Papers
    Refatoração de alinhamento do cadastro de endereço

--- Zit.AgencyManager.Web/obj/Debug/net8.0/Zit.AgencyManager.Web.AssemblyInfo.cs
+++ Zit.AgencyManager.Web/obj/Debug/net8.0/Zit.AgencyManager.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d04d4304ccf2db6a9e47aa81998d06ddd57042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1b3a3bfc71517fff0fea0c2251a050431c8592")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
