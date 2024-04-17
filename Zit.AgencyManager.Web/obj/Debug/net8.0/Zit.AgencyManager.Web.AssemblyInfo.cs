468734cf74c8f3a5de24787c8bca1a613bb83d01
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2024-04-16 22:47:50 -0300

    Refatorando Layout de Login

--- Zit.AgencyManager.Web/obj/Debug/net8.0/Zit.AgencyManager.Web.AssemblyInfo.cs
+++ Zit.AgencyManager.Web/obj/Debug/net8.0/Zit.AgencyManager.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390e9fa847fb1e47824481e90b9675fe610bf2c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160b13dd130f8a5dfbf990f12b6f896b89999b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
