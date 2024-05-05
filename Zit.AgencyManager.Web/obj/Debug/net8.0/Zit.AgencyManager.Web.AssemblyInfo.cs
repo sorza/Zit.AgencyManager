37889541a279575447b5544a1ecaf0792e40285c
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2024-05-04 23:39:16 -0300

    Correção da soma das vendas

--- Zit.AgencyManager.Web/obj/Debug/net8.0/Zit.AgencyManager.Web.AssemblyInfo.cs
+++ Zit.AgencyManager.Web/obj/Debug/net8.0/Zit.AgencyManager.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4dd22cc69c8dafa3d34b81f8af36fa88f61e754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c632fece01c9634f90391219f86265773e69f66a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zit.AgencyManager.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
